e1003697326ba9613bbc5af8da1c52c2b1f16e0c
JorgeMichaelPerezVasquez <jperezv8@upao.edu.pe>
2023-05-16 23:33:40 -0500

    Creaciòn de la estructura del proyecto y definicion de clases.

---
+++ mentoring-api/src/test/java/com/technomentoring/mentoringapi/MentoringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technomentoring.mentoringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
