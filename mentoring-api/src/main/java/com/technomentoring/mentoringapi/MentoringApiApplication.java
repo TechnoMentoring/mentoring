85d7f45c5a1140dbb7a3fe369b25110f9b6fc652
EHGCCODE <hannover.tano@gmail.com>
2023-05-17 02:25:21 -0500

    feat: Creacion de estructura del proyecto y definicion de clases e interfaces.

---
+++ mentoring-api/src/main/java/com/technomentoring/mentoringapi/MentoringApiApplication.java
@@ -0,0 +1,13 @@
+package com.technomentoring.mentoringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentoringApiApplication.class, args);
+    }
+
+}
