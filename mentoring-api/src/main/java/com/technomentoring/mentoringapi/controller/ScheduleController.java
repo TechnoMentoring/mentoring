54e99d5fb7ff67cf9c5e7367f3f0e7ccc8e97bc3
Ludies29 <vdiaze1@upao.edu.pe>
2023-07-01 04:57:34 -0500

    feat: creacion de clases dto y modl de request

--- mentoring-api/src/main/java/com/technomentoring/mentoringapi/controller/ScheduleController.java
+++ mentoring-api/src/main/java/com/technomentoring/mentoringapi/controller/ScheduleController.java
@@ -1,8 +1,18 @@
 package com.technomentoring.mentoringapi.controller;
 
+import com.technomentoring.mentoringapi.dto.MentorDTO;
+import com.technomentoring.mentoringapi.dto.RequestDTO;
 import com.technomentoring.mentoringapi.dto.ScheduleDTO;
+import com.technomentoring.mentoringapi.dto.UsuariosDTO;
+import com.technomentoring.mentoringapi.model.Mentor;
+import com.technomentoring.mentoringapi.model.Request;
 import com.technomentoring.mentoringapi.model.Schedule;
+import com.technomentoring.mentoringapi.model.Usuarios;
 import com.technomentoring.mentoringapi.service.IScheduleService;
+import com.technomentoring.mentoringapi.service.IUsuariosService;
+import com.technomentoring.mentoringapi.service.impl.RequestServiceImpl;
+import com.technomentoring.mentoringapi.service.impl.SchedulerServiceImpl;
+import com.technomentoring.mentoringapi.service.impl.UsuariosServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -11,14 +21,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
-@RequestMapping("/schedule")
+@RequestMapping("/user/schedule")
 @RequiredArgsConstructor
 public class ScheduleController {
     private final IScheduleService service;
+    private final UsuariosServiceImpl usuariosService;
+    private final SchedulerServiceImpl schedulerService;
+    private final RequestServiceImpl requestService;
+    private final ModelMapper modelMapper;
 
     @Qualifier("scheduleMapper")
     private final ModelMapper mapper;
@@ -26,10 +41,27 @@ public class ScheduleController {
     private ScheduleDTO convertToDto(Schedule obj){
         return mapper.map(obj,ScheduleDTO.class);
     }
+    private Schedule convertToEntity(ScheduleDTO dto) throws UserPrincipalNotFoundException {
+        Schedule schedule = mapper.map(dto, Schedule.class);
+        Usuarios usuarios = new Usuarios();
+        usuarios.setIdUser(dto.getIdUser()); // Asigna el id de usuario desde el DTO
 
-    private Schedule convertToEntity(ScheduleDTO dto){
-        return mapper.map(dto,Schedule.class);
+        // Carga el objeto Usuarios existente de la base de datos
+        Usuarios existingUser = usuariosService.findById(dto.getIdUser());
+        usuarios.setUsername(existingUser.getUsername());
+        usuarios.setPassword(existingUser.getPassword());
+        usuarios.setName(existingUser.getName());
+        usuarios.setEmail(existingUser.getEmail());
+        usuarios.setDni(existingUser.getDni());
+        usuarios.setProfile(existingUser.getProfile());
+        usuarios.setRoles(existingUser.getRoles());
+
+        schedule.setUsuarios(usuarios); // Establece el objeto Usuarios en el Schedule
+        return schedule;
     }
+
+
+
     @PostMapping("/create")
     public ResponseEntity<ScheduleDTO> create(@Valid @RequestBody ScheduleDTO dto) throws Exception{
         Schedule obj = service.save(convertToEntity(dto));
@@ -66,4 +98,58 @@ public class ScheduleController {
         service.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<ScheduleDTO>> getSchedulesByUserId(@PathVariable("id") Long userId) {
+        List<Schedule> schedules = schedulerService.getSchedulesByUserId(userId);
+        List<ScheduleDTO> scheduleDTOs = schedules.stream()
+                .map(schedule -> modelMapper.map(schedule, ScheduleDTO.class))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(scheduleDTOs, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/getSchedule-Mentor/{scheduleId}")
+    public ResponseEntity<UsuariosDTO> getMentorByRequestAndSchedule(@PathVariable("scheduleId") Integer scheduleId) throws Exception {
+        // Obtener la solicitud por su ID
+        Schedule schedule = service.readById(scheduleId);
+
+        // Verificar si la solicitud existe
+        if (schedule == null) {
+            throw new Exception("el horario no existe");
+        }
+
+        // Obtener el usuario mentor vinculado a la solicitud
+        Usuarios mentor = schedule.getUsuarios();
+
+        // Verificar si el usuario mentor existe
+        if (mentor == null) {
+            throw new Exception("el horario no tiene un usuario mentor asignado");
+        }
+
+        // Mapear el usuario mentor a un DTO para devolverlo en la respuesta
+        UsuariosDTO mentorDTO = mapper.map(mentor, UsuariosDTO.class);
+
+        return new ResponseEntity<>(mentorDTO, HttpStatus.OK);
+    }
+
+///////////////////////////////Muestra la solicitudes de un mentor
+    @GetMapping("/getRequestsMentor/{idUser}")
+    public ResponseEntity<List<RequestDTO>> getRequestsByUserId(@PathVariable("idUser") Long idUser) {
+        List<Schedule> schedules = schedulerService.getSchedulesByUserId(idUser);
+        List<Integer> scheduleIds = schedules.stream()
+                .map(Schedule::getIdSchedule)
+                .collect(Collectors.toList());
+
+        List<Request> requests = requestService.getRequestsByScheduleIds(scheduleIds);
+        List<RequestDTO> requestDTOs = requests.stream()
+                .map(request -> modelMapper.map(request, RequestDTO.class))
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(requestDTOs, HttpStatus.OK);
+    }
+
+
+
+
 }
--- mentoring-api/src/main/java/com/technomentoring/mentoringapi/service/IScheduleService.java
+++ mentoring-api/src/main/java/com/technomentoring/mentoringapi/service/IScheduleService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public interface IScheduleService extends ICRUD<Schedule, Integer> {
     boolean isScheduleDuplicate(String title);
-    boolean existMentorById(Integer idMentor);
-    boolean existStudentById(Integer idStudent);
+    boolean existUsuariosById(Long idUser);
     List<Schedule> findScheduleByTitle(String title);
 }
